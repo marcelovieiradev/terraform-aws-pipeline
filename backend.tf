fdcce6404c741fa1afc9dec90a05962d1fc0a365
marcelo-vieira <marcelobv.dev@gmail.com>
2026-04-14 09:08:52 -0300

    fix: corrige backend s3 e adiciona policies OIDC

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket  = "meu-bucket-marcelov"
-    key     = "marcelo-site/terraform.tsstate"
+    key     = "marcelo-site/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
 
